c4a2aafc0523799442e7b54af6e85805cb194596
Alex Luyster <77421475+luystera@users.noreply.github.com>
2026-08-12 18:54:12 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # brute force solution would be to sort the strings and compare
+
+        # 2 hashmaps for tracking character counts in the strings
+        s_map = {}
+        t_map = {}
+        # then compare character counts in each
+
+        for schar,tchar in zip(s,t): 
+            s_map[schar] = s_map.get(schar, 0) + 1
+            t_map[tchar] = t_map.get(tchar, 0) + 1
+        
+        # now check if the frequencies are equal
+        return s_map == t_map
